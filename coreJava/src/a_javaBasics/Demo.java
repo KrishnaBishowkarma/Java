e19daa8280cde85a4464f5654efe2a51451e5ba3
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-02-27 11:23:46 +0545

    Initial Commit

---
+++ coreJava/src/a_javaBasics/Demo.java
@@ -0,0 +1,16 @@
+package a_javaBasics;
+
+public class Demo {
+	// Main method
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		//Calling demo method 
+		demo();
+	}
+	
+	// Demo method
+	public static void demo() {
+		System.out.println("\nHello Nepal!");
+	}
+}
