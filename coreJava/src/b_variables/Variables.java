2fd2b40705e8315f058d98123edf64bf3d62dd8d
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-02-27 13:55:46 +0545

    Added Variables

---
+++ coreJava/src/b_variables/Variables.java
@@ -0,0 +1,21 @@
+package b_variables;
+
+public class Variables {
+	public static void main(String[] args) {
+		
+		// variable declaration
+		byte a;
+	
+		// variable initialization
+		a = 10;
+		
+		// Variable declaration and initialization
+		int b = 20;
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+		int sum = a+b;
+		System.out.println("\nSum of a and b is = " + sum);
+	}
+}
