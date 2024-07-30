c78136ad669853086dac2d7de47f82269379a55b
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-07-30 12:34:57 +0545

    Arithmetic Operator in Java

---
+++ src/com/krishna/operators/ArithmeticOperator.java
@@ -0,0 +1,24 @@
+package com.krishna.operators;
+
+public class ArithmeticOperator {
+    public static void main(String[] args) {
+        int numberOne = 100;
+        int numberTwo = 15;
+
+        // Addition +
+        int sum = numberOne + numberTwo;
+        System.out.println("The sum of two numbers is : " + sum);
+
+        // Subtraction -
+        int difference = numberOne - numberTwo;
+        System.out.println("The difference between tow numbers is : " + difference);
+
+        // Multiplication *
+        int multiplication = numberOne * numberTwo;
+        System.out.println("The multiplication of two numbers is : " + multiplication);
+
+        // Division /
+        int division  = numberOne / numberTwo;
+        System.out.println("The division of two numbers is : " + division);
+    }
+}
