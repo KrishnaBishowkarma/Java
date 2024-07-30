ea14c57de0799e2e20a00c13d0d9652b7f6450a6
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-07-30 14:41:04 +0545

    Decrement Operator (--)

---
+++ src/com/krishna/operators/unaryOperator/decrement/PostDecrement.java
@@ -0,0 +1,11 @@
+package com.krishna.operators.unaryOperator.decrement;
+
+public class PostDecrement {
+    public static void main(String[] args) {
+        int a = 10;
+
+        System.out.println("Value of a before decrement: " + a--);
+
+        System.out.println("Value of a after decrement: " + a);
+    }
+}
