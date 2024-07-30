73068a4111fef7b95d75ef4375ed15913769358b
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-07-30 14:40:28 +0545

    Increment Operator (++)

---
+++ src/com/krishna/operators/unaryOperator/increment/PostIncrement.java
@@ -0,0 +1,11 @@
+package com.krishna.operators.unaryOperator.increment;
+
+public class PostIncrement {
+    public static void main(String[] args) {
+        int a = 10;
+
+        System.out.println("Value of a before increment: " + a++);
+
+       System.out.println("Value of a after increment: " + a);
+    }
+}
