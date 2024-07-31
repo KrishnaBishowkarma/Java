832c635e29d858abb661343c324b2455012e04c9
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-07-31 14:37:07 +0545

    Logical Operator in Java

---
+++ src/com/krishna/operators/logicalOperator/NOT_Operator.java
@@ -0,0 +1,13 @@
+package com.krishna.operators.logicalOperator;
+
+public class NOT_Operator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        // if the condition is false, the operation returns true and when the condition is true, the operation returns false.
+
+        System.out.println("!(a<=b) : " + !(a<=b));
+        System.out.println("!(a>=b) : " + !(a>=b));
+    }
+}
