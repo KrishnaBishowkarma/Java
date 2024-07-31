739b1791352d66694111bbdc60b30c352a518d99
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-07-31 14:08:34 +0545

    Comparision Operator

---
+++ src/com/krishna/operators/comparisionOperator/IsGreaterThanOperator.java
@@ -0,0 +1,13 @@
+package com.krishna.operators.comparisionOperator;
+
+public class IsGreaterThanOperator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        System.out.println("Is a greater than b : " + (a > b));
+    }
+}
