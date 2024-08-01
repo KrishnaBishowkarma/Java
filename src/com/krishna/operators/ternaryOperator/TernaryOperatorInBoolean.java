e32771c82af139a8a83a88bae55464fc56e688bf
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-08-01 23:54:23 +0545

    Ternary Operator in Java

---
+++ src/com/krishna/operators/ternaryOperator/TernaryOperatorInBoolean.java
@@ -0,0 +1,10 @@
+package com.krishna.operators.ternaryOperator;
+
+public class TernaryOperatorInBoolean {
+    public static void main(String[] args) {
+        boolean condition = true;
+        String result = (condition) ? "True" : "False";
+
+        System.out.println(result);
+    }
+}
