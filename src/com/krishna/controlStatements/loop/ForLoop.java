6332fb4c1ae6ec53a1ade5c4a39b983c6f7e945c
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-08-06 21:02:16 +0545

    Control Statements - For Loop

---
+++ src/com/krishna/controlStatements/loop/ForLoop.java
@@ -0,0 +1,10 @@
+package com.krishna.controlStatements.loop;
+
+public class ForLoop {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Meow");
+        }
+    }
+}
