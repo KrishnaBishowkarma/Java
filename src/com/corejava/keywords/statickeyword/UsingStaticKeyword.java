2788746c3371c574604dcc0617afc0505d125cf1
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-08-10 14:15:14 +0545

    Static Keyword

---
+++ src/com/corejava/keywords/statickeyword/UsingStaticKeyword.java
@@ -0,0 +1,29 @@
+package com.corejava.keywords.statickeyword;
+
+public class UsingStaticKeyword {
+    // Static Variable
+    static int count = 0;
+
+    // Static block
+    static {
+        System.out.println("Hello from Static block!");
+    }
+
+    // Static Method
+    static void incrementCount() {
+        count++;
+        System.out.println("Count : " + count);
+    }
+
+    public static void main(String[] args) {
+        // Accessing static variable directly using the class name
+        System.out.println("Initial count : " + UsingStaticKeyword.count);
+
+        // Modifying the static variable
+        UsingStaticKeyword.count += 5;
+
+        // Accessing static method directly using the class name
+        UsingStaticKeyword.incrementCount();
+
+    }
+}
---
+++ src/com/corejava/keywords/statickeyword/WithoutUsingStaticKeyword.java
@@ -0,0 +1,30 @@
+package com.corejava.keywords.statickeyword;
+
+public class WithoutUsingStaticKeyword {
+    // Instance Variable
+    int count = 0;
+
+    // Constructor (which is called when an object of the class is created)
+    public WithoutUsingStaticKeyword() {
+        System.out.println("Hello from Constructor");
+    }
+
+    // Instance Method
+    void incrementCount() {
+        count++;
+        System.out.println("Count : " + count);
+    }
+
+    public static void main(String[] args) {
+        // Create an instance of the class (constructor called)
+        WithoutUsingStaticKeyword obj = new WithoutUsingStaticKeyword();
+
+        // Accessing the instance variable using object
+        System.out.println("Initial Count : " + obj.count);
+
+        // Modifying the instance variable
+        obj.count += 5;
+
+        obj.incrementCount();
+    }
+}
