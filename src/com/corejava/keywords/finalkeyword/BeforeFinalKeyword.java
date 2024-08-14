492ba7c44c127e26c7d7133cfc58102ee6d0423c
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-08-14 22:06:30 +0545

    Using final keyword to create constant variable

---
+++ src/com/corejava/keywords/finalkeyword/BeforeFinalKeyword.java
@@ -0,0 +1,9 @@
+package com.corejava.keywords.finalkeyword;
+
+public class BeforeFinalKeyword {
+    public static void main(String[] args) {
+        var name = "Alex";
+        name = "Purni";
+        System.out.println(name);
+    }
+}
---
+++ src/com/corejava/keywords/finalkeyword/BestApproachToUseFinalKeyword.java
@@ -0,0 +1,10 @@
+package com.corejava.keywords.finalkeyword;
+
+public class BestApproachToUseFinalKeyword {
+    public static final String Name = "Krishna";
+
+    public static void main(String[] args) {
+        // Name = "Riyanshi";
+        System.out.println();
+    }
+}
