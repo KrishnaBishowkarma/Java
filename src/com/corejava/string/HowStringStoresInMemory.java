0f67ae0945a9936086e01caf8b2dd8becb2d98e7
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-08-15 21:06:47 +0545

    String

---
+++ src/com/corejava/string/HowStringStoresInMemory.java
@@ -0,0 +1,8 @@
+package com.corejava.string;
+
+public class HowStringStoresInMemory {
+    public static void main(String[] args) {
+        String name1 = "Jamila";
+        String name2 = "Jamila";
+    }
+}
