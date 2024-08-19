9963ef22a4d647f0ebbec29be325f4e0048bd499
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-08-19 22:11:33 +0545

    Exception in java

---
+++ src/com/corejava/exception/UsingORWithinCatch.java
@@ -0,0 +1,11 @@
+package com.corejava.exception;
+
+public class UsingORWithinCatch {
+    public static void main(String[] args) {
+        int number = Integer.parseInt("1");
+
+        for(int i = 10; i <= 0; i--){
+            System.out.println();
+        }
+    }
+}
