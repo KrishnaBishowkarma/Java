169ad3f1c4711060bdba87cb267f082c03441181
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-08-21 20:41:11 +0545

    Catching all different exceptions and using finally block

---
+++ src/com/corejava/exception/FinallyBlock.java
@@ -0,0 +1,14 @@
+package com.corejava.exception;
+
+public class FinallyBlock {
+    public static void main(String[] args) {
+        try {
+            int number = Integer.parseInt("1");
+            System.out.println("Number : %d" + number);
+        } catch (NumberFormatException e) {
+            System.out.println("Failed to parse 1x as integer");
+        } finally {
+            System.out.println("Finally always runs!");
+        }
+    }
+}
