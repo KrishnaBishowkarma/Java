5f4230d2e77bda6377858d5a371bc3fbb8cf11cd
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-08-25 23:06:50 +0545

    Using Getter method in class and object

---
+++ src/com/corejava/classesandobjects/getter/Main.java
@@ -0,0 +1,15 @@
+package com.corejava.classesandobjects.getter;
+
+public class Main {
+    public static void main(String[] args) {
+        // Object
+        Dog tommy = new Dog();
+        tommy.setName("Tommy");
+        System.out.println(tommy.getName());
+
+        // Object
+        Dog jimmy = new Dog();
+        jimmy.setName("Jimmy");
+        System.out.println(jimmy.getName());
+    }
+}
