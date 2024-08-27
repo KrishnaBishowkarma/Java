92f6ad1f6ee2aca5bcdbc85876ae25918b4160eb
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-08-27 15:08:45 +0545

    Using multiple constructor based on specific need

---
+++ src/com/corejava/constructors/usingmultiple/Baby.java
@@ -0,0 +1,55 @@
+package com.corejava.constructors.usingmultiple;
+
+public class Baby {
+    private String gender;
+    private String name;
+    private int age;
+
+    // Default Constructor
+    public Baby() {
+        // Initialize default values if necessary
+        this.gender = "Unknown";
+        this.name = "Unknown";
+        this.age = 0;
+    }
+
+    // Parameterized Constructor with limited parameters
+    public Baby(String gender) {
+        this.gender = gender;
+        this.name = "Unknown";
+        this.age = 0;
+    }
+
+    // Parameterized Constructor with all parameters
+    public Baby(String gender, String name, int age) {
+        this.gender = gender;
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getters
+    public String getGender() {
+        return gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
