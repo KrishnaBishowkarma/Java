9265a9eac48a74c65f4dea10a88f9abe63c5110b
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-08-31 21:16:35 +0545

    Library Management System - Core Java Project

---
+++ src/com/projects/librarymanagementsystem/Library.java
@@ -0,0 +1,41 @@
+package com.projects.librarymanagementsystem;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Library {
+    private ArrayList<Book> books;
+
+    public Library() {
+        books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void viewBooks() {
+        if (books.isEmpty()) {
+            System.out.println("No books available in the library.");
+        } else {
+            for (Book book : books) {
+                System.out.println(book);
+            }
+        }
+    }
+
+    public Book searchBook(String title) {
+        for (Book book : books) {
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                return book;
+            }
+        }
+        return null;
+    }
+
+    public boolean removeBook(String isbn) {
+        return books.removeIf(book -> book.getIsbn().equals(isbn));
+    }
+}
+
