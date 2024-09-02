8aaeaa5571c38138ec0e5a58b1705fd74771aca5
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-09-02 22:12:09 +0545

    using if else to check largest number

---
+++ src/com/practice/controlstatement/LargestOfThree.java
@@ -0,0 +1,15 @@
+package com.practice.controlstatement;
+
+public class LargestOfThree {
+    public static void main(String[] args) {
+        int num1 = 10, num2 = 20, num3 = 30;
+
+        if (num1 >= num2 && num1 >= num3) {
+            System.out.println("The largest number is: " + num1);
+        } else if (num2 >= num1 && num2 >= num3) {
+            System.out.println("The largest number is: " + num2);
+        } else {
+            System.out.println("The largest number is: " + num3);
+        }
+    }
+}
