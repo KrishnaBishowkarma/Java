d66e00b696be494508ac2834c0148be133469bcf
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-09-04 08:29:24 +0545

    another example of using constructors, getters, setters, and toString method

---
+++ src/com/corejava/constructors/example2/Cat.java
@@ -0,0 +1,49 @@
+package com.corejava.constructors.example2;
+
+public class Cat {
+    private String name;
+    private int age;
+    private String colour;
+
+    public Cat() {
+    }
+
+    public Cat(String name, int age, String colour) {
+        this.name = name;
+        this.age = age;
+        this.colour = colour;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", colour='" + colour + '\'' +
+                '}';
+    }
+}
---
+++ src/com/corejava/constructors/example2/Gender.java
@@ -0,0 +1,6 @@
+package com.corejava.constructors.example2;
+
+public enum Gender {
+    MALE,
+    FEMALE
+}
