3c4bee565ebadd4a0c268c63d3a629f31b2bbb37
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-09-05 22:03:27 +0545

    Structuring a person in program

---
+++ src/com/practice/exercise/Address.java
@@ -0,0 +1,61 @@
+package com.practice.exercise;
+
+import java.util.Objects;
+
+public class Address {
+    private String firstLine;
+    private String postalCode;
+    private String country;
+
+    public Address(String firstLine, String postalCode, String country) {
+        this.firstLine = firstLine;
+        this.postalCode = postalCode;
+        this.country = country;
+    }
+
+    public String getFirstLine() {
+        return firstLine;
+    }
+
+    public void setFirstLine(String firstLine) {
+        this.firstLine = firstLine;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(firstLine, address.firstLine) && Objects.equals(postalCode, address.postalCode) && Objects.equals(country, address.country);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstLine, postalCode, country);
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "firstLine='" + firstLine + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+}
---
+++ src/com/practice/exercise/CarBrand.java
@@ -0,0 +1,10 @@
+package com.practice.exercise;
+
+public enum CarBrand {
+    TESLA,
+    BMW,
+    FERRARI,
+    BUGATTI,
+    ROLLSROYCE,
+    THAR,
+}
