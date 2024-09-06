0e302a1907596e4d46a09d5a68743d11cfe19810
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-09-06 19:49:25 +0545

    Understanding static keyword

---
+++ src/com/corejava/keywords/statickeyword2/Car.java
@@ -0,0 +1,52 @@
+package com.corejava.keywords.statickeyword2;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Car {
+    private CarBrand car;
+    private BigDecimal price;
+
+
+    public Car(CarBrand car, BigDecimal price) {
+        this.car = car;
+        this.price = price;
+    }
+
+    public CarBrand getCar() {
+        return car;
+    }
+
+    public void setCar(CarBrand car) {
+        this.car = car;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car1 = (Car) o;
+        return car == car1.car && Objects.equals(price, car1.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(car, price);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "car=" + car +
+                ", price=" + price +
+                '}';
+    }
+}
---
+++ src/com/corejava/keywords/statickeyword2/CarBrand.java
@@ -0,0 +1,9 @@
+package com.corejava.keywords.statickeyword2;
+
+public enum CarBrand {
+    TESLA,
+    BUGATTI,
+    BMW,
+    FERRARI,
+    ROLLSROYCE
+}
