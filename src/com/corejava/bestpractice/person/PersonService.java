f1e5cffeb968336804bc115ce0d53cb592ba9be2
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-09-11 21:39:33 +0545

    Utility Classes in Java

--- src/com/corejava/bestpractice/person/PersonService.java
+++ src/com/corejava/bestpractice/person/PersonService.java
@@ -1,8 +1,10 @@
 package com.corejava.bestpractice.person;
 
+import static com.corejava.bestpractice.utils.StringUtils.isEmpty;
+
 public class PersonService {
     public int addPerson(Person person) {
-        if (person.getName().isBlank()) {
+        if (isEmpty(person.getName())) {
             throw new IllegalArgumentException("Name cannot be blank");
         }
         // Store person to Database
---
+++ src/com/corejava/bestpractice/utils/StringUtils.java
@@ -0,0 +1,8 @@
+package com.corejava.bestpractice.utils;
+
+public class StringUtils {
+    public static boolean isEmpty(String str){
+        // this is pointless but mainly for example
+        return str.isEmpty();
+    }
+}
