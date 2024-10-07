e96ee1734650b7d1b29fb07d2e3158cfaa749240
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-10-07 07:49:17 +0545

    Implementation of Map interface using TreeMap

---
+++ src/com/dsa/datastructures/maps/MapWithTreeMap.java
@@ -0,0 +1,19 @@
+package com.dsa.datastructures.maps;
+
+import java.util.TreeMap;
+import java.util.Map;
+
+public class MapWithTreeMap {
+    public static void main(String[] args) {
+        // Create a TreeMap
+        Map<Integer, String> treeMap = new TreeMap<>();
+
+        // Add key-value pairs to the TreeMap
+        treeMap.put(3, "Cherry");
+        treeMap.put(1, "Apple");
+        treeMap.put(2, "Banana");
+
+        // Print the map in sorted order (based on keys)
+        System.out.println("TreeMap: " + treeMap);
+    }
+}
