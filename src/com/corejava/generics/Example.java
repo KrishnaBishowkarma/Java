46f6e43fbcea03618426ff08b5592328765865b9
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-10-08 21:36:45 +0545

    Generics in Java

---
+++ src/com/corejava/generics/Example.java
@@ -0,0 +1,16 @@
+package com.corejava.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Example {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList();
+        numbers.add(1);
+//        numbers.add("2");
+
+        for (Object number : numbers) {
+            System.out.println((Integer) number);
+        }
+    }
+}
