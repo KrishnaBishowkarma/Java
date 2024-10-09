17e81adb25a476fee40ae51ad8acb750c3ab81c7
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-10-09 16:24:31 +0545

    Generics Example 2

---
+++ src/com/corejava/generics/Example2.java
@@ -0,0 +1,13 @@
+package com.corejava.generics;
+
+public class Example2 {
+    public static void main(String[] args) {
+//        Comparable number = 10;
+//        number.compareTo("10");
+
+        Comparable<Integer> number = 10;
+        System.out.println(number.compareTo(10));
+        System.out.println(number.compareTo(11));
+        System.out.println(number.compareTo(9));
+    }
+}
