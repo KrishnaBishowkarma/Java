1a790d3c78e70b238018e0bd5855c3339f281251
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-10-09 16:34:29 +0545

    Type parameter and type argument

---
+++ src/com/corejava/generics/GenericType.java
@@ -0,0 +1,19 @@
+package com.corejava.generics;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GenericType {
+    public static void main(String[] args) {
+//      List<E>
+        List<String> names = new ArrayList<String>();
+        List<Integer> numbers = new ArrayList<Integer>();
+
+        Map<String, String> map = new HashMap<>();
+        Map<Point, Double> map2 = new HashMap<>();
+
+    }
+}
