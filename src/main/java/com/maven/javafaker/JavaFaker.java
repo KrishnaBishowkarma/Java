7eaae57c0254eb7255df1f0185f916ffce9fb2ab
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-10-21 20:19:12 +0545

    organizing the directory for maven projects

--- src/main/java/com/maven/javafaker/JavaFaker.java
+++ src/main/java/com/maven/javafaker/JavaFaker.java
@@ -1,8 +1,8 @@
-package com.maven.initial;
+package com.maven.javafaker;
 
 import com.github.javafaker.Faker;
 
-public class FirstDemo {
+public class JavaFaker {
     public static void main(String[] args) {
         Faker faker = new Faker();
         System.out.println(faker.name().fullName());
