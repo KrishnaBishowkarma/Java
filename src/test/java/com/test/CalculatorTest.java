9f3aad69f6f7b061ca4ec6d69706f08215bfb2ca
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-10-22 20:54:06 +0545

    adding some tests

--- src/test/java/com/test/CalculatorTest.java
+++ src/test/java/com/test/CalculatorTest.java
@@ -8,14 +8,28 @@ public class CalculatorTest {
     @Test
     void canAddTwoNumbers() {
         Calculator underTest = new Calculator();
-        int number1 = 10;
-        int number2 = 20;
+        var number1 = 10;
+        var number2 = 20;
 
-        int result = underTest.add(number1, number2);
+        var result = underTest.add(number1, number2);
 
         assertEquals(result, 30);
 
     }
 
+    @Test
+    void canHandleWhenInputIsZero() {
+        Calculator underTest = new Calculator();
+        var result = underTest.add(0);
+        assertEquals(0, result);
+    }
+
+    @Test
+    void canAddNumbersFromAGivenArray() {
+        Calculator underTest = new Calculator();
+        var numbers = new int[]{1, 21, 3};
+        var result = underTest.add(numbers);
+        assertEquals(25, result);
+    }
 
 }
