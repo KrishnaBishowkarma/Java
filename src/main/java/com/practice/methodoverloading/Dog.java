1506283a6c6ca665f8054854b2ed8b284368bd2b
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-10-24 21:32:11 +0545

    adding Dog.java of method overloading practice file

---
+++ src/main/java/com/practice/methodoverloading/Dog.java
@@ -0,0 +1,17 @@
+package com.practice.methodoverloading;
+
+public class Dog {
+    public void bark(){
+        System.out.println("The dog is barking");
+    }
+
+    public void bark(int times){
+        for (int i = 0; i < times; i++){
+            System.out.println("Woof!);");
+        }
+    }
+
+    public void bark(String sound){
+        System.out.println("The dog says: " + sound);
+    }
+}
