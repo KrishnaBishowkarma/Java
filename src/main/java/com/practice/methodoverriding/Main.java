f74e73e1c04eae610262be3db50eead06b12978a
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-10-25 21:22:29 +0545

    adding main class for method-overriding

---
+++ src/main/java/com/practice/methodoverriding/Main.java
@@ -0,0 +1,17 @@
+package com.practice.methodoverriding;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create an Instrument object
+        Instrument generalInstrument = new Instrument();
+        generalInstrument.play();
+
+        // Create a Guitar object
+        Instrument guitar = new Guitar(); // Polymorphic reference
+        guitar.play();
+
+        // Create a Piano object
+        Instrument piano = new Piano(); // Polymorphic reference
+        piano.play();
+    }
+}
