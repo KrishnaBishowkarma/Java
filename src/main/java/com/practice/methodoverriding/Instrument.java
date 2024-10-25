39d20eca80bcbac12012d6c1cf51a36030e1a37a
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-10-25 21:20:08 +0545

    Adding superclass for method overriding

---
+++ src/main/java/com/practice/methodoverriding/Instrument.java
@@ -0,0 +1,9 @@
+package com.practice.methodoverriding;
+
+// Superclass Instrument
+class Instrument {
+    // Method to be overridden by subclasses
+    public void play() {
+        System.out.println("The instrument plays a tune.");
+    }
+}
