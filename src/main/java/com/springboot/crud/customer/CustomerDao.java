f5710e3c85aaa822ea39940629580dc93cd81db1
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-11-02 10:56:32 +0545

    defined CustomerDao interface with method signatures for data access (DAO Layer)

---
+++ src/main/java/com/springboot/crud/customer/CustomerDao.java
@@ -0,0 +1,11 @@
+package com.springboot.crud.customer;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CustomerDao {
+    List<Customer> selectAllCustomers();
+
+    Optional<Customer> selectCustomerById(Integer id);
+
+}
