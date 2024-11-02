32306f26a33a39b0f9a75029ca7a218814b0a30c
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-11-02 10:46:23 +0545

    adding customer class for customer related fields

---
+++ src/main/java/com/springboot/crud/customer/Customer.java
@@ -0,0 +1,76 @@
+package com.springboot.crud.customer;
+
+import java.util.Objects;
+
+public class Customer {
+    private Integer id;
+    private String name;
+    private String email;
+    private int age;
+
+    public Customer() {
+    }
+
+    public Customer(Integer id, String name, String email, int age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return age == customer.age && Objects.equals(id, customer.id) && Objects.equals(name, customer.name) && Objects.equals(email, customer.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
