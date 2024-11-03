0806937b31cf72a62ea9672969fe701a0d169452
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-11-03 06:57:09 +0545

    Creating custom beans

--- src/main/java/com/springboot/crud/Main.java
+++ src/main/java/com/springboot/crud/Main.java
@@ -1,11 +1,9 @@
 package com.springboot.crud;
 
-import com.springboot.crud.customer.CustomerController;
-import com.springboot.crud.customer.CustomerDataAccessService;
-import com.springboot.crud.customer.CustomerService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
@@ -22,13 +20,31 @@ public class Main {
 
 //        System.out.println(Customers);
 
-
+        // Spring IoC Container (Inspecting the beans managed by Spring)
         ConfigurableApplicationContext applicationContext =
                 SpringApplication.run(Main.class, args);
 
         String[] beanDefinitionNames =
                 applicationContext.getBeanDefinitionNames();
 
+        for (String beanDefinitionName : beanDefinitionNames) {
+            System.out.println(beanDefinitionName);
+        }
+
+        printBeans(applicationContext);
+    }
+
+    @Bean
+    public Foo getFoo() {
+        return new Foo("bar");
+    }
+
+    record Foo(String name) {
+    }
+
+    // Custom method to print beans
+    private static void printBeans(ConfigurableApplicationContext ctx) {
+        String[] beanDefinitionNames = ctx.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
             System.out.println(beanDefinitionName);
         }
