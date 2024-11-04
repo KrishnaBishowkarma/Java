be10b921420e6964ebb353a5d9592c54cac9df6f
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-11-04 18:07:24 +0545

    adding CustomerRepository class, responsible for interacting with the database

---
+++ src/main/java/com/springboot/crud/customer/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.springboot.crud.customer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository
+        extends JpaRepository<Customer, Integer> {
+}
