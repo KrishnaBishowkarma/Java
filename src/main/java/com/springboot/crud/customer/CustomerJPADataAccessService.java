b1606a53fd50e2d38a4a7ef940817d5825b60807
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-11-04 18:05:27 +0545

    adding CustomerJPADataAccessService class to manage database operations

---
+++ src/main/java/com/springboot/crud/customer/CustomerJPADataAccessService.java
@@ -0,0 +1,26 @@
+package com.springboot.crud.customer;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository("jpa")
+public class CustomerJPADataAccessService implements CustomerDao {
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerJPADataAccessService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public List<Customer> selectAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Optional<Customer> selectCustomerById(Integer id) {
+        return Optional.empty();
+    }
+}
