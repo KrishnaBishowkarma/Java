90849fd06df6aeb0ff8ef63e7b7c158e45f9a9c4
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-11-04 17:16:27 +0545

    turning Customer class into an Entity

--- src/main/java/com/springboot/crud/customer/Customer.java
+++ src/main/java/com/springboot/crud/customer/Customer.java
@@ -1,8 +1,23 @@
 package com.springboot.crud.customer;
 
+import jakarta.persistence.*;
+
 import java.util.Objects;
 
+@Entity
 public class Customer {
+
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence"
+    )
+
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
+
     private Integer id;
     private String name;
     private String email;
