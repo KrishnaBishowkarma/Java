dc70630aabc216a8c3e346614342e67ec86de3e7
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-11-05 12:07:09 +0545

    removing custom Beans creation to avoid any conflict with our main application

--- src/main/java/com/springboot/crud/Main.java
+++ src/main/java/com/springboot/crud/Main.java
@@ -9,44 +9,6 @@ import org.springframework.context.annotation.Bean;
 public class Main {
 
     public static void main(String[] args) {
-
-        /*
-        Never Do this
-        CustomerService customerService =
-                new CustomerService(new CustomerDataAccessService());
-        CustomerController customerController =
-                new CustomerController(customerService);
-        */
-
-//        System.out.println(Customers);
-
-        // Spring IoC Container (Inspecting the beans managed by Spring)
-        ConfigurableApplicationContext applicationContext =
-                SpringApplication.run(Main.class, args);
-
-        String[] beanDefinitionNames =
-                applicationContext.getBeanDefinitionNames();
-
-        for (String beanDefinitionName : beanDefinitionNames) {
-            System.out.println(beanDefinitionName);
-        }
-
-        printBeans(applicationContext);
-    }
-
-    @Bean
-    public Foo getFoo() {
-        return new Foo("bar");
-    }
-
-    record Foo(String name) {
-    }
-
-    // Custom method to print beans
-    private static void printBeans(ConfigurableApplicationContext ctx) {
-        String[] beanDefinitionNames = ctx.getBeanDefinitionNames();
-        for (String beanDefinitionName : beanDefinitionNames) {
-            System.out.println(beanDefinitionName);
-        }
+        SpringApplication.run(Main.class, args);
     }
 }
