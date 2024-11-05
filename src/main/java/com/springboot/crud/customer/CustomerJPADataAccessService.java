bcc6af5b7a584499648a069cc8df9228bb3fccf7
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-11-05 12:39:36 +0545

    changing Optional to findById method to get result based on Id

--- src/main/java/com/springboot/crud/customer/CustomerJPADataAccessService.java
+++ src/main/java/com/springboot/crud/customer/CustomerJPADataAccessService.java
@@ -21,6 +21,6 @@ public class CustomerJPADataAccessService implements CustomerDao {
 
     @Override
     public Optional<Customer> selectCustomerById(Integer id) {
-        return Optional.empty();
+        return customerRepository.findById(id);
     }
 }
